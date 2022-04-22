a7c9b8453a166b153527b08959cb00389da73fbd
sumamr <99878446+sumamr@users.noreply.github.com>
2022-04-22 16:19:24 +0530

    Add files via upload

---
+++ DBConnect.java
@@ -0,0 +1,34 @@
+package suma.com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+	static String driver="com.mysql.cj.jdbc.Driver";
+	static String url="jdbc:mysql://localhost:3306/mydb";
+	static String un="root";
+	static String pass="root";
+	static Connection con=null;
+	
+	
+	public static Connection getConnection()
+	{
+		try
+		{
+			Class.forName(driver);
+			con=DriverManager.getConnection(url, un, pass);
+			if(con==null)
+			{
+				System.out.println("Connection object is null");
+			}
+			
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();;
+		}
+		return con;
+	}
+
+}
